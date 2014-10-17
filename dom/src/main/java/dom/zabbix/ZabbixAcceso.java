f89dd3c521509c1debe7171a9901e7e3f338b704
munozdaniel <munozda87@hotmail.com>
2014-10-17 20:27:18 -0300

    Agregando metodo getHost y getApp

--- dom/src/main/java/dom/zabbix/ZabbixAcceso.java
+++ dom/src/main/java/dom/zabbix/ZabbixAcceso.java
@@ -2,15 +2,39 @@ package dom.zabbix;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.codehaus.jackson.JsonNode;
 
 public class ZabbixAcceso {
 	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 	private String password;
 	private String userName;
+	private String zabbixApiUrl;
 	public String authenticate(String username, String password, String url) {
 	    this.password = password;
 	    this.userName = username;
-	    String zabbixApiUrl = url;
+	    this.zabbixApiUrl = url;
 	 
 	    StringBuilder uiConnectMessage = new StringBuilder();
 	    StringBuilder sb = new StringBuilder();
@@ -46,5 +70,74 @@ public class ZabbixAcceso {
 	    httpPost.setEntity(new StringEntity(request));
 	    httpPost.addHeader("Content-Type", "application/json-rpc");
 	    return client.execute(httpPost);
+	}
+	
+	/***
+	 * 
+	 */
+	private Map<String, String> hostMap = new HashMap<String, String>();
+	private Map<String, Map<String, String>> appMap = new HashMap<String, Map<String, String>>();
+	private Object auth;
+	private Map<String, String> mapper = new HashMap<String, JsonNode>();
+	 
+	private void getHosts() throws IOException {
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("{\"jsonrpc\":\"2.0\",");
+	    sb.append("\"method\":\"host.get\",");
+	    sb.append("\"params\":{");
+	    sb.append("\"output\":\"extend\"},");
+	    sb.append(" \"auth\":\"").append(auth).append("\",");
+	    sb.append("  \"id\":2}");
+	 
+	    HttpResponse response = postAndGet(sb.toString());
+	    HttpEntity entity = response.getEntity();
+	 
+	    JsonNode rootNode = mapper.readValue(EntityUtils.toString(entity), JsonNode.class);
+	    JsonNode resultNode = rootNode.path("result");
+	    Iterator hostList = resultNode.getElements();
+	    while (hostList.hasNext()) {
+	        JsonNode element = hostList.next();
+	        hostMap.put(element.findValue("hostid").toString().replaceAll("(\\[|\\]|\")", ""), element.findValue("host")
+	            .toString().replaceAll("(\\[|\\]|\")", ""));
+	    }
+	}
+	 
+	private void getApps() throws IOException {
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("{\"jsonrpc\":\"2.0\",");
+	    sb.append("\"method\":\"application.get\",");
+	    sb.append("\"params\":{");
+	    sb.append("\"output\":\"extend\"},");
+	    sb.append(" \"auth\":\"").append(auth).append("\",");
+	    sb.append("  \"id\":2}");
+	 
+	    HttpResponse response = postAndGet(sb.toString());
+	    HttpEntity entity = response.getEntity();
+	 
+	    JsonNode rootNode = mapper.readValue(EntityUtils.toString(entity), JsonNode.class);
+	    JsonNode resultNode = rootNode.path("result");
+	    Iterator appList = resultNode.getElements();
+	    while (appList.hasNext()) {
+	        JsonNode element = appList.next();
+	        JsonNode hosts = element.path("hosts");
+	        Iterator hostList = hosts.getElements();
+	        while (hostList.hasNext()) {
+	            JsonNode hostElement = hostList.next();
+	            String hostID = hostElement.findValue("hostid").toString().replaceAll("(\\[|\\]|\")", "");
+	            if (hostMap.containsKey(hostID)) {
+	                if (appMap.containsKey(hostID)) {
+	                    appMap.get(hostElement.findValue("hostid").toString().replaceAll("(\\[|\\]|\")", "")).put(element.
+	                        findValue("name").toString().replaceAll("(\\[|\\]|\")", ""), element.findValue("applicationid").
+	                        toString().replaceAll("(\\[|\\]|\")", ""));
+	 
+	                } else {
+	                    Map apps = new HashMap();
+	                    apps.put(element.findValue("name").toString().replaceAll("(\\[|\\]|\")", ""), element.
+	                        findValue("applicationid").toString().replaceAll("(\\[|\\]|\")", ""));
+	                    appMap.put(hostID, apps);
+	                }
+	            }
+	        }
+	    }
 	}
 }
