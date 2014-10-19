fd00851636680896db25085d14f6cebfbf25c9b1
munozdaniel <munozda87@hotmail.com>
2014-10-19 15:50:54 -0300

    ZabbixAutenticacion, metodo obtener token por ip.

--- dom/src/main/java/dom/zabbix/ZabbixAutenticacion.java
+++ dom/src/main/java/dom/zabbix/ZabbixAutenticacion.java
@@ -40,4 +40,38 @@ public class ZabbixAutenticacion {
 		}
 		return null;
 	}
+
+	public static String obtenerTokenPorIp(final String ip) {
+		try {
+
+			JSONObject mainJObj = new JSONObject();
+			JSONObject paramJObj = new JSONObject();
+
+			mainJObj.put("jsonrpc", "2.0");
+			mainJObj.put("method", "user.login");
+
+			paramJObj.put("user", "Admin");
+			paramJObj.put("password", "zabbix");
+
+			mainJObj.put("params", paramJObj);
+			mainJObj.put("id", "1");
+
+			Webb webb = Webb.create();
+
+			JSONObject result = webb
+					.post("http://" + ip + "/zabbix/api_jsonrpc.php")
+					.header("Content-Type", "application/json")
+					.useCaches(false).body(mainJObj).ensureSuccess()
+					.asJsonObject().getBody();
+
+			return result.getString("result");
+
+		} catch (JSONException je) {
+
+			System.out.println("Error creating JSON request to Zabbix API..."
+					+ je.getMessage());
+
+		}
+		return null;
+	}
 }
