77dbbbfaef608480b89af45cdaa9fc3a364ff389
munozdaniel <munozda87@hotmail.com>
2014-10-19 19:54:45 -0300

    Agregado HddItem

---
+++ dom/src/main/java/dom/zabbix/monitoreo/HddItem.java
@@ -0,0 +1,23 @@
+package dom.zabbix.monitoreo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+
+public class HddItem extends ItemManager {
+
+	@Override
+	protected void cargarParametros() {
+		Map<String, String> cpu = new HashMap<String, String>();
+		cpu.put("key_", "system.cpu.load");//Renombrar
+		try {
+			this.getParametrosJson().put("search", cpu);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
