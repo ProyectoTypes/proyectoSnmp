1f47b6e4d51b22daec23faf616bab90cf4e2b4da
munozdaniel <munozda87@hotmail.com>
2014-10-19 16:48:04 -0300

    ZabbixManager: Agregado constante HOST y comentarios.

--- dom/src/main/java/dom/zabbix/ZabbixManager.java
+++ dom/src/main/java/dom/zabbix/ZabbixManager.java
@@ -5,12 +5,18 @@ import org.json.JSONObject;
 import com.goebl.david.Webb;
 
 public abstract class ZabbixManager {
-
+	protected static final String HOST = "inventario";
+	/**
+	 * Constructor
+	 */
 	public ZabbixManager() {
 		this.objetoJson = new JSONObject();
 		this.parametrosJson = new JSONObject();
 	}
-
+	
+	/*
+	 * Atributos
+	 */
 	private JSONObject objetoJson;
 
 	public JSONObject getObjetoJson() {
@@ -30,18 +36,28 @@ public abstract class ZabbixManager {
 	public void setParametrosJson(JSONObject parametrosJson) {
 		this.parametrosJson = parametrosJson;
 	}
-
+	/* FIN: Atributos
+	*/
+	/*
+	 * OPERACIONES
+	 */
+	/**
+	 * Permite obtener el token a partir de la autenticacion en Zabbix.
+	 * @param ip
+	 * @return
+	 */
 	protected String obtenerToken(final String ip) {
 		return ZabbixAutenticacion.obtenerTokenPorIp(ip);
 	}
-	protected JSONObject ejecutarJson()
-	{
-		Webb webb = Webb.create();
 
-		return webb
-		.post("http://127.0.0.1/zabbix/api_jsonrpc.php")
-		.header("Content-Type", "application/json").useCaches(false)
-		.body(getObjetoJson()).ensureSuccess().asJsonObject().getBody();
+	protected JSONObject ejecutarJson() {
+		Webb webb = Webb.create();
+		// FIXME: Verificar la direccion, hace referencia a la dir del Servidor.
+		// El superUser deberia setearla ?
+		
+		return webb.post("http://127.0.0.1/zabbix/api_jsonrpc.php")
+				.header("Content-Type", "application/json").useCaches(false)
+				.body(getObjetoJson()).ensureSuccess().asJsonObject().getBody();
 
 	}
 
