cd4789738d41c0900e99a48d2f1bfe30dd2274bb
munozdaniel <munozda87@hotmail.com>
2014-10-19 19:55:04 -0300

    Agregado RamItem

---
+++ dom/src/main/java/dom/zabbix/monitoreo/RamItem.java
@@ -0,0 +1,22 @@
+package dom.zabbix.monitoreo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+
+public class RamItem extends ItemManager {
+
+	@Override
+	protected void cargarParametros() {
+		Map<String, String> cpu = new HashMap<String, String>();
+		cpu.put("key_", "system.cpu.load");//Renombrar la key para cada caso.
+		try {
+			this.getParametrosJson().put("search", cpu);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
