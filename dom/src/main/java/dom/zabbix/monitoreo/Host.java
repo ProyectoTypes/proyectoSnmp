391d27969d481d211a6e70cfb5423bb4014207d4
munozdaniel <munozda87@hotmail.com>
2014-10-19 16:15:04 -0300

    Host: devuelve el id del host.

--- dom/src/main/java/dom/zabbix/monitoreo/Host.java
+++ dom/src/main/java/dom/zabbix/monitoreo/Host.java
@@ -1,7 +1,45 @@
 package dom.zabbix.monitoreo;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+
 import dom.zabbix.ZabbixManager;
 
-public class Host  extends ZabbixManager{
+/**
+ * Administra toda la informacion referida al Host.
+ * 
+ * @author cipoleto
+ * 
+ */
+public class Host extends ZabbixManager {
+
+	/**
+	 * Permite obtener el id del host. Con este id es posible realizar consultas
+	 * sobre datos que genera el host.
+	 * 
+	 * @param token
+	 * @throws JSONException
+	 */
+	public String requestHostId(final String ip)
+			throws JSONException {
+		String token = this.obtenerToken(ip);
+		this.getParametrosJson().put("output", "hostid");
+		this.getParametrosJson().put("selectGroups", "extend");
+		Map<String, String> filtro = new HashMap<String, String>();
+		filtro.put("host", "inventario");
+
+		this.getParametrosJson().put("filter", filtro);
+
+		getObjetoJson().put("params", this.getParametrosJson());
+		getObjetoJson().put("jsonrpc", "2.0");
+		getObjetoJson().put("method", "host.get");
+		getObjetoJson().put("auth", token);
+		getObjetoJson().put("id", "2");	
+		
+		return  this.ejecutarJson().getString("result");
+	}
+	
 
 }
