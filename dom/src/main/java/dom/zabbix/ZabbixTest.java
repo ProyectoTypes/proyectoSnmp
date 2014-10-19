39841ee14bac6d6301f5a6c1550535e5d053fc0f
munozdaniel <munozda87@hotmail.com>
2014-10-19 17:15:33 -0300

    testeando item cpu

--- dom/src/main/java/dom/zabbix/ZabbixTest.java
+++ dom/src/main/java/dom/zabbix/ZabbixTest.java
@@ -2,7 +2,9 @@ package dom.zabbix;
 
 import org.json.JSONException;
 
+import dom.zabbix.monitoreo.CpuItem;
 import dom.zabbix.monitoreo.Host;
+import dom.zabbix.monitoreo.ItemManager;
 
 public class ZabbixTest {
 
@@ -11,6 +13,8 @@ public class ZabbixTest {
 		Host host = new Host();
 		try {
 			System.out.println("HOST ID "+ host.requestHostId("127.0.0.1"));
+			ItemManager cpu = new CpuItem();
+			System.out.println("CPU "+cpu.requestCpuItem("127.0.0.1"));
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			System.out.println("ERROR: "+e.getMessage());
